d2f2d1377d72aa5c861ae90e2d918b2f0011fc87
Philippe Ombredanne <pombredanne@nexb.com>
2015-05-12 17:52:22 +0200

    Bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from version import get_version
 
 setup(
     name='libarchive-c',
-    version='3.0.1',
+    version='3.1.0',
     description='Python interface to libarchive',
     author='Changaco',
     author_email='changaco@changaco.oy.lc',
